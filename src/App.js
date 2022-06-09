6ca89593e3ea3540721ac0257636b90045ec5f44
sabbir10s <sabbirahmed1023@gmail.com>
2022-06-09 17:51:37 +0600

    footer section added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
 
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Home />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='bg-primary text-base-300 py-10'>
+            <p className='text-center'>&copy; 2022 <a className='text-secondary font-bold' href="https://www.linkedin.com/in/sabbir10s/" target="_blank">Sabbir Ahmed</a> All rights reserved.</p>
+        </div>
+    );
+};
+
+export default Footer;
